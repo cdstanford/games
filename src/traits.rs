400629a6eb56b502a8d42a38fec8a7aab3b6c6fd
Caleb Stanford <caleb.pirsquared@gmail.com>
2021-05-15 21:26:38 -0400

    trait for abstract Game

--- src/traits.rs
+++ src/traits.rs
@@ -1,6 +1,12 @@
 /*
-    View trait:
-    abstracts the behavior of an object that has both a public and a private view.
+    General traits
+
+    - View trait:
+      Abstracts the behavior of an object that has both a public and a private view.
+
+    - Game trait:
+      Abstracts the behavior of a game with hidden information
+      (like Stratego or Battleship)
 */
 
 pub trait View {
@@ -12,4 +18,29 @@ pub trait View {
     fn disp_priv(&self) -> String;
     /// Display public view
     fn disp_pub(&self) -> String;
+}
+
+pub enum GameStatus<Player> {
+    ToMove(Player),
+    Won(Player),
+}
+
+pub trait Game {
+    type Player;
+    type Move;
+
+    /// Who is to move, or (if the game is ended) who has won
+    fn status(&self) -> GameStatus<Self::Player>;
+    fn is_ended(&self) -> bool {
+        match self.status() {
+            GameStatus::ToMove(_) => false,
+            GameStatus::Won(_) => true,
+        }
+    }
+
+    /// Whether a move is valid
+    fn valid_move(&self, mv: Self::Move) -> bool;
+
+    /// Making the move
+    fn make_move(&mut self, mv: Self::Move);
 }
