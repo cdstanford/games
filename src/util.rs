2f377e0507d251acc653473dfa0e2255ffe339f3
Caleb Stanford <caleb.pirsquared@gmail.com>
2021-05-17 00:04:42 -0400

    expand generic user input utilities

--- src/util.rs
+++ src/util.rs
@@ -3,6 +3,7 @@
 */
 
 use std::io::{self, BufRead};
+use std::str::FromStr;
 
 pub fn user_input(query: &str) -> String {
     print!("{}", query);
@@ -12,4 +13,29 @@ pub fn user_input(query: &str) -> String {
         .next()
         .expect("failed to get line from stdin")
         .expect("failed to get line from stdin")
+}
+
+pub fn from_user_input<T: FromStr>(query: &str, query_again: &str) -> T {
+    let mut result = user_input(query).parse().ok();
+    while result.is_none() {
+        result = user_input(query_again).parse().ok();
+    }
+    result.unwrap()
+}
+
+pub fn from_user_input_satisfying<T, F>(
+    query: &str,
+    query_again: &str,
+    query_invalid: &str,
+    predicate: F,
+) -> T
+where
+    T: FromStr,
+    F: Fn(&T) -> bool,
+{
+    let mut result = from_user_input(query, query_again);
+    while !predicate(&result) {
+        result = from_user_input(query_invalid, query_again);
+    }
+    result
 }
